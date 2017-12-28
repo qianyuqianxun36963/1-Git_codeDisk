135f0c57fb47134352fda3881d210b50d95d708d
qianyuqianxun36963 <1521318763@qq.com>
2017-12-28 17:46:44 +0800

    1228
    1228

--- 2-java/1-java基础/src/java语言/b_数据/类型/字符串类/字符串转码再比较.java
+++ 2-java/1-java基础/src/java语言/b_数据/类型/字符串类/字符串转码再比较.java
@@ -9,29 +9,81 @@ public class 字符串转码再比较 {
     public void test() throws UnsupportedEncodingException{
         String defaultString = "字符串";
         
-        String str_utf8 = new String("字符串".getBytes(),"UTF-8");
+        String str_utf8 = new String("字符串".getBytes(),"utf8");
         String str_gb2312 = new String("字符串".getBytes(),"gb2312");
+        String str_gbk = new String("字符串".getBytes(),"gbk");
         
-        if(checkStringEqual(defaultString, str_utf8)){
+        
+//        if(equals("", null)){
+//            System.out.println("字符串相等");
+//        }else{
+//            System.out.println("字符串不相等");
+//        }
+//        
+//        if(equals("", "")){
+//            System.out.println("字符串相等");
+//        }else{
+//            System.out.println("字符串不相等");
+//        }
+//        
+//        if(equals(null, null)){
+//            System.out.println("字符串相等");
+//        }else{
+//            System.out.println("字符串不相等");
+//        }
+        
+        if(equals(defaultString, str_utf8)){
+            System.out.println("字符串相等");
+        }else{
+            System.out.println("字符串不相等");
+        }
+        
+        if(equals(defaultString, str_gb2312)){
+            System.out.println("字符串相等");
+        }else{
+            System.out.println("字符串不相等");
+        }
+        
+        if(equals(defaultString, str_gbk)){
             System.out.println("字符串相等");
         }else{
             System.out.println("字符串不相等");
         }
     }
     
-    public boolean checkStringEqual(String a,String b){
-        String stra;
-        try {
-            stra = new String(a.getBytes(),"UTF-8");
-            String strb = new String(b.getBytes(),"UTF-8");
-            return stra.equals(strb);
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return false;
-        }
-        
+    public static boolean equals2(String str1, String str2) {
+        return str1 == null ? str2 == null : str1.equals(str2);
     }
+    
+    public static boolean equals(String str1, String str2) throws UnsupportedEncodingException {
+        String str1_utf8 = toUTF8(str1);
+        String str2_utf8 = toUTF8(str2);
+        return str1 == null ? str2 == null : str1_utf8.equals(str2_utf8);
+    }
+    
+    public static String toUTF8(String str) throws UnsupportedEncodingException{
+        if(str != null) {
+            String strCode = getEncoding(str).toString();
+            return new String(str.getBytes(strCode),"utf8");
+        }
+        return null;
+    }
+    
+    //获取字符串编码不成功。。下面代码没用！
+    public static String getEncoding(String str) {  
+        String encode[] = new String[]{ "GBK","ISO-8859-1", "GB2312", "UTF-8", "GB18030", "Big5", "Unicode", "ASCII" };  
+        for (int i = 0; i < encode.length; i++){  
+            try {  
+                String temp = new String(str.getBytes(encode[i]), encode[i]);
+                if (str.equals(temp)) {  
+                    return encode[i];  
+                }  
+            } catch (Exception ex) {  
+            }  
+        }  
+        return "";  
+    }  
+    
 }
 
 
