c44b8a20d8e057e6ccafa45f29a9bed92cbe43aa
wangyajun <1521318763@qq.com>
2018-03-21 23:40:26 +0800

    0321
    0321

---
+++ 5-springboot/myspringboot/simple/src/main/java/simple/demo/controller/serverletController/serverletController.java
@@ -0,0 +1,13 @@
+package simple.demo.controller.serverletController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import simple.demo.controller.baseController;
+
+@Controller
+public class serverletController extends baseController {
+    @RequestMapping(value="/hello")
+    public String say(){
+        return "hello";
+    }
+}
