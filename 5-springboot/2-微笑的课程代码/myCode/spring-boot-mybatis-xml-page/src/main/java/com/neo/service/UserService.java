3d5d8568a6567e79290052662b5d6ea8efd91cc5
qianyuqianxun36963 <1521318763@qq.com>
2018-10-29 23:31:58 +0800

    1029

---
+++ 5-springboot/2-微笑的课程代码/myCode/spring-boot-mybatis-xml-page/src/main/java/com/neo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.neo.service;
+
+import com.github.pagehelper.PageHelper;
+import com.neo.entity.UserEntity;
+import com.neo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    public  List<UserEntity> getAll() {
+        PageHelper.startPage(0,2);
+        List<UserEntity>  users = userMapper.getAll();
+        return users;
+    }
+}
