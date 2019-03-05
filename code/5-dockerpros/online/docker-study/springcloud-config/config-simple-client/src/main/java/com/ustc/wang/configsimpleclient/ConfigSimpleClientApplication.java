a696e32ecabb743ffefdb7d5d9d143b771effc12
qianyuqianxun36963 <1521318763@qq.com>
2019-03-06 01:25:34 +0800

    0306

---
+++ code/5-dockerpros/online/docker-study/springcloud-config/config-simple-client/src/main/java/com/ustc/wang/configsimpleclient/ConfigSimpleClientApplication.java
@@ -0,0 +1,13 @@
+package com.ustc.wang.configsimpleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSimpleClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSimpleClientApplication.class, args);
+	}
+
+}
---
+++ code/5-dockerpros/online/docker-study/springcloud-config/config-simple-client/src/test/java/com/ustc/wang/configsimpleclient/ConfigSimpleClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ustc.wang.configsimpleclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigSimpleClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
